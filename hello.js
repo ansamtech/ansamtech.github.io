df952c83ae98f9bb9bbb159adcb119fb42e50ccd
ansamtech <63403416+ansamtech@users.noreply.github.com>
2022-01-02 18:55:48 +0530

    Create hello.js

---
+++ hello.js
@@ -0,0 +1,3 @@
+function qwerty() {
+  document.getElementById("qwertyp").innerHTML = "also visit <a href='https://www.ansamtech.xyz/2021/12/pdf-trueman-2022-biology-class-11-12-free-pdf.html'>trueman biology 2022 pdf</a> ";
+}
